be6baa66408816d2147643f903ccdf27cc499009
Burak Yazan <brkyznn@gmail.com>
2022-06-02 21:21:26 +0300

    install axios and api configs

---
+++ src/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+import config from "./config";
+
+const api = axios.create({
+  baseURL: `${config.API_BASE_URL}${config.API_API_VERSION}`
+})
+
+export default api;
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = Object.freeze({
+  API_BASE_URL : 'https://6298ed4dbf77b6025823f16f.mockapi.io',
+  API_API_VERSION: '/api/v1'
+})
+
+export default config;
