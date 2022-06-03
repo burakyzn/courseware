c4ad7e0ffcd9f94de2fded8fd3ffb20c7d048448
Burak Yazan <brkyznn@gmail.com>
2022-06-03 10:54:26 +0300

    fix typo

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import config from "./config";
 
 const api = axios.create({
-  baseURL: `${config.API_BASE_URL}${config.API_API_VERSION}`
+  baseURL: `${config.API_BASE_URL}${config.API_VERSION}`
 })
 
 export default api;
