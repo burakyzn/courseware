ecbf7b5429a18db3b363071bf2035c73a0795340
Burak Yazan <brkyznn@gmail.com>
2022-06-03 22:33:07 +0300

    create CategorySlice & add it to store

--- src/app/store.js
+++ src/app/store.js
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import basketReducer from '~features/BasketSlice';
 import tabBarReducer from '~features/TabBarSlice';
 import authReducer from '~features/AuthSlice';
+import categoryReducer from '~features/CategorySlice';
 
 const store = configureStore({
   reducer: {
     tabBar: tabBarReducer,
     basket: basketReducer,
-    auth: authReducer
+    auth: authReducer,
+    category : categoryReducer
   },
 });
 
